5d2028696d92068444bb768cb1e957f1dec685eb
Charles Assuncao <junioassuncaocharles@gmail.com>
2018-03-18 11:26:21 -0300

    Add option to merge a existing obj

--- normalize.test.js
+++ normalize.test.js
@@ -25,3 +25,40 @@ test('should return a normalized object by id', () => {
 
   expect(normalize(array, 'id')).toEqual(normalizedObject);
 });
+
+test('should return a normalized object by id and merged', () => {
+  const array = [
+    {
+      id: '123131321313',
+      name: 'name1'
+    },
+    {
+      id: '728292',
+      name: 'name2'
+    },
+  ];
+
+  const prevObj = {
+    2234: {
+      id: '2234',
+      name: 'name1'
+    }
+  };
+
+  const normalizedObject = {
+    123131321313: {
+      id: '123131321313',
+      name: 'name1'
+    },
+    728292: {
+      id: '728292',
+      name: 'name2'
+    },
+    2234: {
+      id: '2234',
+      name: 'name1'
+    }
+  };
+
+  expect(normalize(array, 'id', prevObj)).toEqual(normalizedObject);
+});
